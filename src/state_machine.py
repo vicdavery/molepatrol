888c6f93769e3f490f867f884e22e9706f9457d8
Dave Vickery <davidvickery01@gmail.com>
2014-03-31 16:39:21 +0100

    Add state machine tests and complete state machine

--- src/state_machine.py
+++ src/state_machine.py
@@ -1,21 +1,61 @@
+#    +-------------------------------------+
+#    |                                     |
+#    |                                     V
+#  IDLE <---> PATROLLING --------> RETURNING TO BASE ---> CHARGING
+#    ^        ^        ^                   ^                 |
+#    |        |        |                   |                 |
+#    |        V        V                   |                 |
+#    |    SCARING  SCANNING----------------+                 |
+#    |        |                            |                 |
+#    |        +----------------------------+                 |
+#    +-------------------------------------------------------+
+
+class InvalidEventException(Exception):
+    def __init__(self):
+        self.__name__ = "Invalid Event Exception"
+    def __str__(self):
+        return repr("Cannot handle this event at the moment")
 
 class State(object):
     IDLE = 0
     PATROLLING = 1
-    CHARGING = 2
-    SCARING = 3
-    SCANNING = 4
+    RETURNING_TO_BASE = 2
+    CHARGING = 3
+    SCARING = 4
+    SCANNING = 5
 
 class Event(object):
     START_PATROL = 0
+    ARRIVED_AT_MOLEHILL = 1
+    DONE_SCARING = 2
+    LOW_BATTERY = 3
+    FULLY_CHARGED = 4
+    ARRIVED_AT_BASE = 5
+    SCANNING_TIME = 6
+    DONE_SCANNING = 7
+    DONE_PATROLLING = 8
 
 class StateMachine(object):
-    machine = { State.IDLE : {Event.START_PATROL : State.PATROLLING} }
+    machine = { State.IDLE : {Event.START_PATROL : State.PATROLLING,
+                              Event.LOW_BATTERY : State.RETURNING_TO_BASE},
+                State.PATROLLING : {Event.ARRIVED_AT_MOLEHILL : State.SCARING,
+                                    Event.SCANNING_TIME : State.SCANNING,
+                                    Event.LOW_BATTERY : State.RETURNING_TO_BASE,
+                                    Event.DONE_PATROLLING : State.IDLE},
+               State.RETURNING_TO_BASE : {Event.ARRIVED_AT_BASE : State.CHARGING},
+               State.CHARGING : {Event.FULLY_CHARGED : State.IDLE},
+               State.SCARING : {Event.DONE_SCARING : State.PATROLLING,
+                                Event.LOW_BATTERY : State.RETURNING_TO_BASE},
+               State.SCANNING : {Event.DONE_SCANNING : State.PATROLLING,
+                                 Event.LOW_BATTERY : State.RETURNING_TO_BASE}}
     def __init__(self):
         self.state = State.IDLE
 
     def handle_event(self, event):
-        self.state = StateMachine.machine[self.state][event]
+        try:
+            self.state = StateMachine.machine[self.state][event]
+        except KeyError as e:
+            raise InvalidEventException()
 
     def get_state(self):
         return self.state
--- test/state_machine_test.py
+++ test/state_machine_test.py
@@ -1,5 +1,5 @@
 import unittest
-from state_machine import State, Event, StateMachine
+from state_machine import State, Event, StateMachine, InvalidEventException
 
 
 class StateMachineCreationTestCase(unittest.TestCase):
@@ -7,22 +7,319 @@ class StateMachineCreationTestCase(unittest.TestCase):
         sm = StateMachine()
         self.assertEqual(sm.get_state(), State.IDLE, "When created should be in IDLE state.")
         self.assertNotEqual(sm.get_state(), State.PATROLLING)
+        self.assertNotEqual(sm.get_state(), State.RETURNING_TO_BASE)
         self.assertNotEqual(sm.get_state(), State.CHARGING)
         self.assertNotEqual(sm.get_state(), State.SCARING)
         self.assertNotEqual(sm.get_state(), State.SCANNING)
 
-    def testIdleToPatrolling(self):
+    #
+    # IDLE STATE
+    #
+    def testIdle_StartPatrol(self):
         sm = StateMachine()
         sm.handle_event(Event.START_PATROL)
         self.assertEqual(sm.get_state(), State.PATROLLING)
 
-    def testIdleToCharging(self):
+    def testIdle_ArrivedAtMolehill(self):
+        sm = StateMachine()
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_MOLEHILL)
+
+    def testIdle_DoneScaring(self):
+        sm = StateMachine()
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCARING)
+
+    def testIdle_LowBattery(self):
         sm = StateMachine()
         sm.handle_event(Event.LOW_BATTERY)
-        self.assertEqual(sm.get_state, State.CHARGING)
+        self.assertEqual(sm.get_state(), State.RETURNING_TO_BASE)
+
+    def testIdle_FullyCharged(self):
+        sm = StateMachine()
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.FULLY_CHARGED)
+
+    def testIdle_ArrivedAtBase(self):
+        sm = StateMachine()
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_BASE)
+
+    def testIdle_ScanningTime(self):
+        sm = StateMachine()
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.SCANNING_TIME)
+
+    def testIdle_DoneScanning(self):
+        sm = StateMachine()
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCANNING)
+
+    def testIdle_DonePatrolling(self):
+        sm = StateMachine()
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_PATROLLING)
+
+    #
+    # PATROLLING
+    #
+    def testPatrolling_StartPatrol(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.START_PATROL)
+
+    def testPatrolling_ArrivedAtMolehill(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        self.assertEqual(sm.get_state(), State.SCARING)
+
+    def testPatrolling_DoneScaring(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        sm.handle_event(Event.DONE_SCARING)
+        self.assertEqual(sm.get_state(), State.PATROLLING)
+
+    def testPatrolling_LowBattery(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertEqual(sm.get_state(), State.RETURNING_TO_BASE)
+
+    def testPatrolling_FullyCharged(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.FULLY_CHARGED)
+
+    def testPatrolling_ArrivedAtBase(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_BASE)
+
+    def testPatrolling_ScanningTime(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        self.assertEqual(sm.get_state(), State.SCANNING)
+
+    def testPatrolling_DoneScanning(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCANNING)
+
+    def testPatrolling_DonePatrolling(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.DONE_PATROLLING)
+        self.assertEqual(sm.get_state(), State.IDLE)
+
+    #
+    # RETURNING TO BASE
+    #
+    def testReturningToBase_StartPatrol(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.START_PATROL)
+
+    def testReturningToBase_ArrivedAtMolehill(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_MOLEHILL)
+
+    def testReturningToBase_DoneScaring(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCARING)
+
+    def testReturningToBase_LowBattery(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.LOW_BATTERY)
+
+    def testReturningToBase_FullyCharged(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.FULLY_CHARGED)
+
+    def testReturningToBase_ArrivedAtBase(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertEqual(sm.get_state(), State.CHARGING)
+
+    def testReturningToBase_ScanningTime(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.SCANNING_TIME)
+
+    def testReturningToBase_DoneScanning(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCANNING)
+
+    def testReturningToBase_DonePatrolling(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_PATROLLING)
+
+    #
+    # CHARGING
+    #
+    def testCharging_StartPatrol(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.START_PATROL)
+
+    def testCharging_ArrivedAtMolehill(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_MOLEHILL)
+
+    def testCharging_DoneScaring(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCARING)
+
+    def testCharging_LowBattery(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.LOW_BATTERY)
+
+    def testCharging_FullyCharged(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        sm.handle_event(Event.FULLY_CHARGED)
+        self.assertEqual(sm.get_state(), State.IDLE)
+
+    def testCharging_ArrivedAtBase(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_BASE)
+
+    def testCharging_ScanningTime(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.SCANNING_TIME)
+
+    def testCharging_DoneScanning(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCANNING)
+
+    def testCharging_DonePatrolling(self):
+        sm = StateMachine()
+        sm.handle_event(Event.LOW_BATTERY)
+        sm.handle_event(Event.ARRIVED_AT_BASE)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_PATROLLING)
+
+    #
+    # SCARING
+    #
+    def testScaring_StartPatrol(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.START_PATROL)
 
 
+    def testScaring_ArrivedAtMolehill(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_MOLEHILL)
 
+    def testScaring_DoneScaring(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        sm.handle_event(Event.DONE_SCARING)
+        self.assertEqual(sm.get_state(), State.PATROLLING)
+
+    def testScaring_LowBattery(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertEqual(sm.get_state(), State.RETURNING_TO_BASE)
+
+    def testScaring_FullyCharged(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.FULLY_CHARGED)
+
+    def testScaring_ArrivedAtBase(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_BASE)
+
+    def testScaring_ScanningTime(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.SCANNING_TIME)
+
+    def testScaring_DoneScanning(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.ARRIVED_AT_MOLEHILL)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCANNING)
+
+    #
+    # SCANNING
+    #
+    def testScanning_StartPatrol(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.START_PATROL)
+
+    def testScanning_ArrivedAtMolehill(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_MOLEHILL)
+
+    def testScanning_DoneScaring(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.DONE_SCARING)
+
+    def testScanning_LowBattery(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        sm.handle_event(Event.LOW_BATTERY)
+        self.assertEqual(sm.get_state(), State.RETURNING_TO_BASE)
+
+    def testScanning_FullyCharged(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.FULLY_CHARGED)
+
+    def testScanning_ArrivedAtBase(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.ARRIVED_AT_BASE)
+
+    def testScanning_ScanningTime(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        self.assertRaises(InvalidEventException, sm.handle_event, Event.SCANNING_TIME)
+
+    def testScanning_DoneScanning(self):
+        sm = StateMachine()
+        sm.handle_event(Event.START_PATROL)
+        sm.handle_event(Event.SCANNING_TIME)
+        sm.handle_event(Event.DONE_SCANNING)
+        self.assertEqual(sm.get_state(), State.PATROLLING)
 
 if __name__ == '__main__':
     unittest.main()
