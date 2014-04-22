3cf8abaa825615e895bf8628b207c013eb77a4f5
Dave Vickery <davidvickery01@gmail.com>
2014-04-22 18:25:52 +0100

    Updated the motor test to be more realistic and implement the non-hardware sections of the class.

--- src/motor.py
+++ src/motor.py
@@ -1,10 +1,32 @@
 
 class Motor(object):
-    LEFT_FRONT = 0
-    LEFT_BACK = 1
-    RIGHT_FRONT = 2
-    RIGHT_BACK = 3
-    def __init__(self, position):
-        self.position = position
+    steps_per_revolution = 32
+
+    def __init__(self):
+       self.position = 0
+    def get_position(self):
+        return self.position
+    def single_step(self, num=1):
+        # Move forward the motor by the required number of steps
+        # ****
+        # Insert the hardware code here
+        # ****
+        #
+
+        # Update the position according to the number of steps moved.
+        self.position = (self.position + num) % Motor.steps_per_revolution
+
+    def single_step_back(self,num=1):
+        # Move backward the motor by the required number of steps
+        # ****
+        # Insert the hardware code here
+        # ****
+        #
+
+        # Update the position according to the number of steps moved.
+        self.position = ((self.position + Motor.steps_per_revolution) - num) % Motor.steps_per_revolution
+
+    def get_steps_per_revolution(self):
+        return Motor.steps_per_revolution
 
 
--- test/motor_test.py
+++ test/motor_test.py
@@ -2,27 +2,51 @@ import unittest
 from motor import Motor
 
 class MotorTestCase(unittest.TestCase):
-    def testRunMotor(self):
-        m = Motor(Motor.LEFT_FRONT)
-    def testForwardOneRevolution(self):
-        None
-    def testBackwardOneRevolution(self):
-        None
-    def testForwardTwoRevolutions(self):
-        None
-    def testBackwardTwoRevolutions(self):
-        None
-    def testForwardHalfRevolution(self):
-        None
-    def testBackwardHalfRevolution(self):
-        None
-    def testForwardSlow(self):
-        None
-    def testBackwardSlow(self):
-        None
-    def testForwardFast(self):
-        None
-    def testBackwardFast(self):
-        None
+    def testForwardOneStep(self):
+        m = Motor()
+        pos = m.get_position()
+        m.single_step()
+        self.assertGreater(m.get_position(), pos)
 
+    def testBackwardOneStep(self):
+        m = Motor()
+        pos = m.get_position()
+        m.single_step_back()
+        self.assertEqual(m.get_position(), Motor.steps_per_revolution - 1)
+
+    def testForwardOneRevolution(self):
+        m = Motor()
+        pos = m.get_position()
+        m.single_step(m.get_steps_per_revolution())
+        self.assertEqual(m.get_position(), pos)
+
+    def testBackwardOneRevolution(self):
+        m = Motor()
+        pos = m.get_position()
+        m.single_step_back(m.get_steps_per_revolution())
+        self.assertEqual(m.get_position(), pos)
+
+    def testForwardTwoRevolutions(self):
+        m = Motor()
+        pos = m.get_position()
+        m.single_step(m.get_steps_per_revolution()*2)
+        self.assertEqual(m.get_position(), pos)
+
+    def testBackwardTwoRevolutions(self):
+        m = Motor()
+        pos = m.get_position()
+        m.single_step_back(m.get_steps_per_revolution()*2)
+        self.assertEqual(m.get_position(), pos)
+
+    def testForwardHalfRevolution(self):
+        m = Motor()
+        pos = m.get_position()
+        m.single_step(m.get_steps_per_revolution()/2)
+        self.assertEqual(m.get_position(), pos+(m.get_steps_per_revolution()/2))
+
+    def testBackwardHalfRevolution(self):
+        m = Motor()
+        pos = m.get_position()
+        m.single_step_back(m.get_steps_per_revolution()/2)
+        self.assertEqual(m.get_position(), pos+(m.get_steps_per_revolution()/2))
 
