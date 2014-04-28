73d9126ad6316a821d8f9634c28d4cd9d15355f1
Dave Vickery <davidvickery01@gmail.com>
2014-04-28 07:32:01 +0100

    Add some comments to the scarer and scanning test files ready to write those tests.

--- test/scanning_test.py
+++ test/scanning_test.py
@@ -2,23 +2,53 @@ import unittest
 
 class ScanningTestCase(unittest.TestCase):
     def testCaptureImage(self):
+        # Grab image
+        # confirm a file has been created
+        # check the filename is correct, e.g. a timestamp.
         None
     def testIdentifyTree(self):
+        # Load image of a tree
+        # Process the image
+        # Check for any identification produced indicates a tree
         None
     def testIdentifyGrass(self):
+        # Load image of just grass
+        # Process the image
+        # Check for any identification produced indicates grass
         None
     def testIdentifyMolehill(self):
+        # Load image of a molehill
+        # Process the image
+        # Check for any identification produced indicates a molehill
         None
     def testIdentifyWater(self):
+        # Load image of the pond
+        # Process the image
+        # Check for any identification produced indicates a pond
         None
     def testIdentifyPerimeter(self):
+        # Load image of fence
+        # Process the image
+        # Check for any identification produced indicates s fence
         None
     def testIdentifyBush(self):
+        # Load image of a bush
+        # Process the image
+        # Check for any identification produced indicates a bush
         None
     def testIdentifyPaving(self):
+        # Load image of the patio
+        # Process the image
+        # Check for any identification produced indicates the patio
         None
     def testIdentifyAnimal(self):
+        # Load image of Max/Daisy
+        # Process the image
+        # Check for any identification produced indicates a dog
         None
     def testIdentifyHuman(self):
+        # Load image of Me/Sarah
+        # Process the image
+        # Check for any identification produced indicates a person
         None
 
--- test/scare_system_test.py
+++ test/scare_system_test.py
@@ -2,8 +2,11 @@ import unittest
 
 
 class ScareSystemTestCase(unittest.TestCase):
-    def testStartScarer(self):
-        None
-    def testStopScarer(self):
+    def testScarer(self):
+        # Check the battery drain first.
+        # Start the scarer
+        # Battery drain should be higher
+        # Stop the scarer
+        # Battery drain should return to normal
         None
 
