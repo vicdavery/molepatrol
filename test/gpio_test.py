54d02dd584963db1b6babdb3e21222ce5f5946ed
Dave Vickery <davidvickery01@gmail.com>
2014-05-12 07:24:46 +0100

    Use "setUp" function to do the GPIO initialisation.

--- test/gpio_test.py
+++ test/gpio_test.py
@@ -4,23 +4,18 @@ from enum import Enum
 from patroller_gpio import Patroller_GPIO
 
 class GPIOTestCase(unittest.TestCase):
+    def setUp(self):
+        if TestSettings.use_mocks:
+            self.gpio = unittest.mock.Mock(spec=['input'], side_effect=[True])
+        else:
+            self.gpio = RPi.GPIO()
 
     def testMotor1Output(self):
-        gpio = None
-        if TestSettings.use_mocks:
-            gpio = unittest.mock.Mock(spec=['input'], side_effect=[True])
-        else:
-            gpio = RPi.GPIO()
-        g = Patroller_GPIO(gpio)
+        g = Patroller_GPIO(self.gpio)
         self.assertTrue(g.high(Patroller_GPIO.Pins.motor_sensor_1))
 
     def testMotor2Output(self):
-        gpio = None
-        if TestSettings.use_mocks:
-            gpio = unittest.mock.Mock(spec=['input'], side_effect=[True])
-        else:
-            gpio = RPi.GPIO()
-        g = Patroller_GPIO(gpio)
+        g = Patroller_GPIO(self.gpio)
         self.assertTrue(g.high(Patroller_GPIO.Pins.motor_sensor_2))
 
     def testMotor1CurrentSensor(self):pass
