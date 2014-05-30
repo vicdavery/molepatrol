160f53718b3327ada10915952c5721f25fbd0c0c
Dave Vickery <davidvickery01@gmail.com>
2014-05-30 16:45:32 +0100

    Add real hardware pins

--- src/patroller_gpio.py
+++ src/patroller_gpio.py
@@ -6,21 +6,48 @@ class Patroller_GPIO(object):
     # This Enum will encapsulate the pins used for various functions. Therefore if we need to rewire this
     # is all that should need changing.
     class Pins(Enum):
-        motor_1_direction = 1
+        motor_1_direction = 17
         motor_1_pwm = 2
-        motor_1_current = 3
-        motor_1_encoder_1 = 4
-        motor_1_encoder_2 = 5
+        motor_1_current = 4
+        motor_1_encoder_1 = 18
+        motor_1_encoder_2 = 23
         motor_1_encoder_combined = 6
-        motor_2_direction = 1
+        motor_2_direction = 22
         motor_2_pwm = 2
-        motor_2_current = 3
-        motor_2_encoder_1 = 4
-        motor_2_encoder_2 = 5
+        motor_2_current = 27
+        motor_2_encoder_1 = 24
+        motor_2_encoder_2 = 25
         motor_2_encoder_combined = 6
 
     def __init__(self):
         # Initialise the pins
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(Pins.motor_1_direction, GPIO.OUT)
+        GPIO.setup(Pins.motor_1_current, GPIO.OUT)
+        GPIO.setup(Pins.motor_2_direction, GPIO.OUT)
+        GPIO.setup(Pins.motor_3_current, GPIO.OUT)
+        GPIO.setup(Pins.motor_1_encoder_1, GPIO.IN)
+        GPIO.setup(Pins.motor_1_encoder_2, GPIO.IN)
+        GPIO.setup(Pins.motor_2_encoder_1, GPIO.IN)
+        GPIO.setup(Pins.motor_2_encoder_2, GPIO.IN)
+
+    def fwd(self):
+        GPIO.output(Pins.motor_1_direction, True)
+        GPIO.output(Pins.motor_2_direction, True)
+        GPIO.output(Pins.motor_1_current, True)
+        GPIO.output(Pins.motor_2_current, True)
+        time.sleep(0.5)
+        GPIO.output(Pins.motor_1_current, False)
+        GPIO.output(Pins.motor_2_current, False)
+
+    def bckwd(self):
+        GPIO.output(Pins.motor_1_direction, False)
+        GPIO.output(Pins.motor_2_direction, False)
+        GPIO.output(Pins.motor_1_current, True)
+        GPIO.output(Pins.motor_2_current, True)
+        time.sleep(0.5)
+        GPIO.output(Pins.motor_1_current, False)
+        GPIO.output(Pins.motor_2_current, False)
 
     def set_high(self, pin): pass
     def set_low(self, pin): pass
